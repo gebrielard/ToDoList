ca5c87d73d564d47e1604ec88afb0f02a33551bb
Gebriela Rifka Duanti <gebrielard@gmail.com>
2020-09-20 19:50:36 +0700

    Added ListView EditText

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> tasks;
+    private ArrayAdapter<String> tasksAdapter;
+    private ListView lvTasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //ListView
+        lvTasks = (ListView) findViewById(R.id.lvTask);
+        tasks = new ArrayList<String>();
+        tasksAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, tasks);
+        lvTasks.setAdapter(tasksAdapter);
+        tasks.add("Send the project");
+        tasks.add("Groceries");
     }
+
 }
